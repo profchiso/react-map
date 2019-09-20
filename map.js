7caff928deae54edc50972a82340da8ea58819a1
Okorie Chinedu sunday <okoriechinedusunday@gmail.com>
2019-09-20 02:55:22 +0100

    remove key

--- map.js
+++ map.js
@@ -183,7 +183,7 @@ export default function App() {
   return(
     <div style={{width:"100vw",height:"100vh"}}>
     <WrappedMap
-    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDfMkchztCunL2meqCyDlsTsu7wvBFTKf0`}
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=`}
     loadingElement={<div style={{height:"100%"}}></div>}
     containerElement={<div style={{height:"100%"}}></div>}
     mapElement={<div style={{height:"100%"}}></div>}
